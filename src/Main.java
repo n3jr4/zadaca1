596ce17e8ee2bbf02e6a21b84cd622a2dfa152b1
Nejra <skandronejra@gmail.com>
2023-03-19 14:40:01 +0100

    Merge remote-tracking branch 'origin/master'

--- src/Main.java
+++ src/Main.java
@@ -41,8 +41,8 @@ public class Main {
 
         int ultimateNumber = 2;
         String ultimateNumberQuestion = "Answer to the Ultimate Question of Life, the Universe, and Everything is the number 42:";
-        boolean ultimateAnswer = true;
-        System.out.println(ultimateNumberQuestion + (ultimateNumber==42));
+        boolean ultimateAnswer = ultimateNumber == 42;
+        System.out.println(ultimateNumberQuestion + (ultimateAnswer));
 
         //   5. zadatak + BONUS
 
